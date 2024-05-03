f89acd11a9eccc377e28949e9af22fd655b925d2
kung036 <sinariari3@gmail.com>
2024-05-03 14:51:57 +0900

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main></main>
+안녕
+<img src="gather-background" alt="" />
+
+<style>
+  img {
+    width: 100vw;
+    height: 100vw;
+  }
+</style>
